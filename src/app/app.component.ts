a9c66d0a7a9dd0553d5c8f81cf3a4ae1d6f1c30c
celong00 <michaelalexanderrustan@gmail.com>
2021-02-23 17:48:45 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  hasil = 0;
+  a = 0;
+  b = 0;
+  tulisanOpen: String;
+  // item = {
+  //   nama: "jeruk",
+  //   harga: 15000,
+  //   satuan: "kg"
+  // };
+  item = [
+    {
+      nama: "jeruk",
+      harga: 15000,
+      satuan: "kg"
+    },
+    {
+      nama: "apel",
+      harga: 5000,
+      satuan: "kg"
+    }
+  ];
+  tampil: boolean = true;
+
+  open() {
+    this.tulisanOpen = "KEBUKAAA";
+    alert("dibuka");
+  }
+
+  values = "";
+  onKey(event: any) {
+    this.values += event.target.value + "|";
+  }
 }
